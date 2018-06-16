b4d64fbea26abdfaa830b7eadab9c1db1c317784
kidumbrella <i.am.kidumbrella@gmail.com>
2018-06-16 15:14:22 +0700

    initial project version

---
+++ app.js
@@ -0,0 +1,21 @@
+//Log to Console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+alert('Я Джаваскрипт!');
+
+
+/*
+multi 
+line
+comment
+*/
+
+
+
